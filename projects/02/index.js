98ae072c2172ae4a018910693d8543202387089d
Hayden Lingle <hayden.lingle@gmail.com>
2021-08-01 10:35:47 -0500

    add 02 files

---
+++ projects/02/index.js
@@ -0,0 +1,91 @@
+// require('dotenv').config();
+// const http = require('http')
+// const fs = require("fs");
+
+// const port = process.env.PORT
+
+// const server = http.createServer((req, res) => {
+//   console.log(req.url)
+//   const path = req.url
+//   if(path == '/'){
+//     res.statusCode = 200
+//     res.setHeader('Content-Type', 'text/html')
+//     fs.readFile('./index.html', 'utf8' , (err, data) => {
+//       if (err) {
+//         console.error(err)
+//       }
+//       res.write(data)
+//       res.end()
+//     })
+//   } else if (path == '/about') {
+//     res.statusCode = 200
+//     res.setHeader('Content-Type', 'text/html')
+//     fs.readFile('./about.html', 'utf8' , (err, data) => {
+//       if (err) {
+//         console.error(err)
+//       }
+//       res.write(data)
+//       res.end()
+//     })
+//   } else if (path == '/contact') {
+//     res.statusCode = 200
+//     res.setHeader('Content-Type', 'text/html')
+//     fs.readFile('./contact-me.html', 'utf8' , (err, data) => {
+//       if (err) {
+//         console.error(err)
+//       }
+//       res.write(data)
+//       res.end()
+//     })
+//   } else {
+//     res.statusCode = 404
+//     res.setHeader('Content-Type', 'text/html')
+//     fs.readFile('./404.html', 'utf8' , (err, data) => {
+//       if (err) {
+//         console.error(err)
+//       }
+//       res.write(data)
+//       res.end()
+//     })
+//   }
+// })
+
+// server.listen(port, () => {
+//   console.log(`Server running at port ${port}`)
+// })
+
+const express = require('express')
+const fs = require("fs");
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+  fs.readFile('./index.html', 'utf8' , (err, data) => {
+    if (err) {
+      console.error(err)
+    }
+    res.send(data)
+  })
+});
+
+app.get('/about', (req, res) => {
+  fs.readFile('./about.html', 'utf8' , (err, data) => {
+    if (err) {
+      console.error(err)
+    }
+    res.send(data)
+  })
+});
+
+app.get('/contact-me', (req, res) => {
+  fs.readFile('./contact-me.html', 'utf8' , (err, data) => {
+    if (err) {
+      console.error(err)
+    }
+    res.send(data)
+  })
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
